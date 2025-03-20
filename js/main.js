f52952ce22a0fa9b3a62c0e7f6d4b5ff8e7d170a
NPendleton1 <nicholas.pendleton10@gmail.com>
2025-03-20 11:49:37 -0500

    nothign

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 // JS scripts placed here
+var tl = gsap.timeline({repeat: 2, repeatDelay: 1});
+tl.to("#el", {x: 100, duration: 1});
+tl.to("#el", {rotation: 50, duration: 1});
+tl.to("#el", {backgroundColor: "red"});
